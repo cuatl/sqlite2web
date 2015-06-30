400af0d61b60e8a9700873f900455709454f734a
Jorge Martínez Mauricio <jorgem@gmail.com>
2015-06-30 11:38:54 -0500

    cordoa-plugin-network-information

--- platforms/android/assets/www/siglo.js
+++ platforms/android/assets/www/siglo.js
@@ -1,23 +1,36 @@
 swf = {}
 swf.app = 20150526;
 swf.tamano=0;
-swf.url = 'http://api.elsiglo.mx/mobile/TorreonApp/newapi.php';
+swf.url = 'http://tar.mx/demos/api.php'; //cambia esto a tu servidor!
 swf.red=0;
 /*
 *  Carga inicial, en cuanto se carga el html (main.html)
 */
 function cargaInicial() {
+   $("#inicio").css('opacity',1);
+   console.log('iniciamos :-)');
    document.addEventListener("deviceready", onDeviceReady, false);
    document.addEventListener("resume", onResume, false);
    document.addEventListener("backbutton", menuback, false);
    //tenemos red?
    setTimeout(function() {
-      if(window.device.model == 'x86_64') { swf.red = 1; }; //emulador
       console.log('RED: '+navigator.connection.type);
    },20*1000);
    //
    $(function() { FastClick.attach(document.body); });
 }
+var menuback = function() {
+   console.log('menuback');
+};
+var onResume = function() {
+   console.log('onResume');
+};
+/* menus {{ */
+var elmenus = function(e) {
+   $(".secciones").css('opacity',0);
+   $('#'+e).css('opacity',1);
+};
+/* }}} */
 /* cuando el dispositivo está listo {{{ */
 function onDeviceReady() {
    console.log('aaa');
