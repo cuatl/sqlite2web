5038f3ba43e51fea54fdabcb48ac0af35a840b48
Jorge Martínez Mauricio <jorgem@gmail.com>
2015-06-30 17:05:59 -0500

    funcion para mostrar mensajes

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Ejemplo de aplicación para capturar información, almacenarla en una base de da
 Esta aplicación está hecha en Apache Cordova (Phonegap) 5.1.1
 
 [Aplicación principal](platforms/android/assets/www/)
+
+Requiere por lo menos estos plugins:
+
+cordova-plugin-network-information 1.0.1 "Network Information" # para ver estatus de red
+cordova-plugin-whitelist 1.0.0 "Whitelist" # por defecto
+cordova-sqlite-storage 0.7.9 "Cordova sqlite storage plugin" # https://github.com/litehelpers/Cordova-sqlite-storage
+
--- platforms/android/assets/www/siglo.js
+++ platforms/android/assets/www/siglo.js
@@ -3,6 +3,7 @@ swf.app = 20150526;
 swf.tamano=0;
 swf.url = 'http://tar.mx/demos/api.php'; //cambia esto a tu servidor!
 swf.red=0;
+swf.mensaje=0; //mensajes en pantalla
 /*
 *  Carga inicial, en cuanto se carga el html (main.html)
 */
@@ -31,8 +32,52 @@ var elmenus = function(e) {
    $('#'+e).show();
 };
 /* }}} */
-/* cuando el dispositivo está listo {{{ */
+/* cuando el dispositivo está listo {{ */
 function onDeviceReady() {
-   console.log('aaa');
+   var db = window.sqlitePlugin.openDatabase({name: "database.db", location: 1});
+   //creamos la tabla1
+   db.transaction(function(tx) {
+      tx.executeSql('CREATE TABLE IF NOT EXISTS tabla1 (id integer primary key, nombre varchar(64) , correo varchar(64), genero integer, nacimiento date)');
+      db.transaction(function(tx) {
+         tx.executeSql("select count(id) as no from tabla1", [], function(tx, res) {
+            console.log(JSON.stringify(res));
+            console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+         });
+      });
+   });
 }
 /* }}} */
+var almacena = function() {
+   //verificamos que llene algunos datos a fuerza...
+   var dd = $("input[name='nombre']");
+   var d = dd.val()||null;
+   if(d === null || d.length < 5) {
+      mensajes('Por favor escriba su nombre completo ··_',3);
+      dd.focus();
+      return false;
+   }
+   var dd =$("input[name='email']");
+   d = dd.val()||null;
+   if(d === null || d.length < 5) { mensajes('Por favor escriba su correo electrónico',3); dd.focus(); return false; }
+   //ahora si almacenamos. Vamos a leer los datos tal como están (todos)
+   $.each( $("input"),function(i,item) {
+      console.log(item);
+   });
+};
+/* muestra un mensaje en pantalla .wait {{{ */
+function mensajes(ms,duracion) {
+   if(ms == 0) {
+      if(swf.mensaje) clearTimeout(swf.mensaje);
+      $("#estado").css('opacity',0).hide();
+      return false;
+   }
+   if(swf.mensaje) { clearTimeout(swf.mensaje); }
+   if(duracion === null || duracion === undefined) duracion=1;
+   if( $("#estado").css('opacity')  == '1') { $("#estado").css('opacity',0).hide(); }
+   $("#estado").html(ms).show().animate({'opacity':1},300);
+   swf.mensaje = setTimeout(function() {
+      $("#estado").animate({'opacity':0},300,'ease-in',function() {
+         $("#estado").hide();
+      }); 
+   },duracion*1000);
+}; /* }}} */
